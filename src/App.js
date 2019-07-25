3118843ad8e0357e6126b3bd574a0f343c0331e4
triggering-event <gabriel.j.shapiro@gmail.com>
2019-07-24 22:20:46 -0400

    imports useState from React in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
-import "./App.css";
-import BottomRow from "./BottomRow";
+import React, {useState} from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
