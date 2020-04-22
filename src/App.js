7b9fd818f14cc41136f2ae2c85352891ebcd51c6
triggering-event <gabriel.j.shapiro@gmail.com>
2020-04-22 00:00:08 -0400

    completes MVP

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
-//TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+    const [homeScore, setHomeScore] = useState(0);
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
+    const [awayScore, setAwayScore] = useState(0);
 
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+    const clickHandler = (event) => {
+        switch (event.target.getAttribute('class')) {
+            case 'homeButtons__touchdown':
+                setHomeScore(homeScore + 6);
+                break;
+            case 'homeButtons__fieldGoal':
+                setHomeScore(homeScore + 3);
+                break;
+            case 'awayButtons__touchdown':
+                setAwayScore(awayScore + 6);
+                break;
+            case 'awayButtons__fieldGoal':
+                setAwayScore(awayScore + 3);
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <div className="container">
+            <section className="scoreboard">
+                <div className="topRow">
+                    <div className="home">
+                        <h2 className="home__name">Lions</h2>
+                        <div className="home__score">{homeScore}</div>
+                    </div>
+                    <div className="timer">00:03</div>
+                    <div className="away">
+                        <h2 className="away__name">Tigers</h2>
+                        <div className="away__score">{awayScore}</div>
+                    </div>
+                </div>
+                <BottomRow/>
+            </section>
+            <section className="buttons">
+                <div className="homeButtons">
+                    <button className="homeButtons__touchdown" onClick={clickHandler}>Home Touchdown</button>
+                    <button className="homeButtons__fieldGoal" onClick={clickHandler}>Home Field Goal</button>
+                </div>
+                <div className="awayButtons">
+                    <button className="awayButtons__touchdown" onClick={clickHandler}>Away Touchdown</button>
+                    <button className="awayButtons__fieldGoal" onClick={clickHandler}>Away Field Goal</button>
+                </div>
+            </section>
         </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
-    </div>
-  );
+    );
 }
 
 export default App;
